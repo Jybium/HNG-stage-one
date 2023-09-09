d4dac2955e593e322f39e5b07a8e8a6a6c0afb82
jybium <asaolujtomi@gmail.com>
2023-09-09 19:09:00 +0100

    Task Completed

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 const DisplayTime = document.querySelector("#Time");
 const DisplayDay = document.querySelector("#Day");
 
+const daysOfTheWeek = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+
 const currentDate = new Date();
 const currentTime = Date.now();
 
-DisplayDay.textContent = ` Day of the week: ${currentDate}`;
-DisplayTime.textContent = `Current UTC: ${currentTime}`;
+const Day = daysOfTheWeek[currentDate.getDay()];
 
-alert(currentDate);
+DisplayDay.textContent = ` DAY OF THE WEEK: ${Day}`;
+DisplayTime.textContent = `CURRENT UTC: ${currentTime}`;
+
+alert(currentDate.toISOString());
 console.log(Date.now());
